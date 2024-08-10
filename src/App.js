01602acda378067da1fb90e3a63885d2ad72caaf
Kush Chaudhari <ckushj@gmail.com>
2024-08-11 00:07:10 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chatbot from './components/chatbot';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Chatbot />
+    </div>
+  );
+}
+
+export default App;
